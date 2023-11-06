e3ccba7186b239a770f6ac4a2ad79379a0f68d0a
MAXKO <macxko5047@hotmail.com>
2023-11-06 20:39:08 +0700

    update All data

---
+++ app/component/camera.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, { useState, useRef } from 'react';
+import Webcam from 'react-webcam';
+
+const App = () => {
+  const webcamRef = useRef<Webcam | null>(null);
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [isCameraOn, setIsCameraOn] = useState(true);
+
+  const handleCapture = () => {
+    const screenshot = webcamRef.current?.getScreenshot();
+    if (screenshot) {
+      setScreenshot(screenshot);
+    }
+  };
+
+  const toggleCamera = () => {
+    setIsCameraOn((prevIsCameraOn) => !prevIsCameraOn);
+  };
+
+  return (
+    <div>
+      <h1>react-webcam</h1>
+      {isCameraOn ? (
+        <Webcam ref={webcamRef} />
+      ) : (
+        <p>Camera is turned off</p>
+      )}
+      <div>
+        <button className='button-33' onClick={toggleCamera}>
+          {isCameraOn ? 'Turn Off Camera' : 'Turn On Camera'}
+        </button>
+      </div>
+      <div>
+        <h2>Screenshot</h2>
+        {isCameraOn && (
+          <div>
+            <button className='button-33' onClick={handleCapture}>Capture</button>
+            {screenshot && <img src={screenshot} alt="Captured" />}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
